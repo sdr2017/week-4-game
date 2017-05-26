33d9da687b7d15740e30fa5ee8355812117e3c91
sdr2017 <sarahjeanlinnebur@Sarah-Linneburs-MacBook-Pro.local>
2017-05-25 23:44:30 -0600

    adding some pseudo code and beginning variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+//I need to generate a random number between 19 and 120
+	//Display this number in the DOM
+
+//I need 4 crystals
+//I need each crystal to generate a random value between 1 and 12
+//One crystal value must always equal 1
+//On click, I need the value of the crystal to be added to the total points
+	//display the total points in the DOM
+
+//If the total points is equal to the random number: 
+	//the DOM says "You win!"
+	//the wins couner is increased by 1
+	//the game resets 
+//If the total points is greater than the random number:
+	//the DOM says "You lose!"
+	//the loss counter is increased by 1
+	//the game resets
+
+$( document ).ready() {
+
+var random number = Math.floor((Math.random() * 120) + 19);
+
+var green;
+var yellow;
+var blue;
+var red;
+
+var wins;
+var losses;
+
+var points;
+
+
+//$("#crystals").on("click", function() {
+
+//}
+
+
+
+
+
+}
