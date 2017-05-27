800f3e56c19aecf245c156ff9bb6b6c89fb121cc
sdr2017 <sarahjeanlinnebur@Sarah-Linneburs-MacBook-Pro.local>
2017-05-26 22:15:16 -0600

    adding crystal values into html value for crystal images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,10 @@
 	//Display this number in the DOM
 
 //I need 4 crystals
-//I need each crystal to generate a random value between 1 and 12
-//One crystal value must always equal 1
+//I need to generate a random value between 1 and 12 for each crystal
+	//One crystal value must always equal 1
+//I need to assign those values to the crystals
+
 //On click, I need the value of the crystal to be added to the total points
 	//display the total points in the DOM
 
@@ -21,7 +23,7 @@ window.onload = function() {
 	var wins;
 	var losses;
 
-	var points;
+	var points = 0;
 
 	var randomNumber = Math.floor((Math.random() * 120) + 19);
 		$("#displayNumber").html("Random Number: " + randomNumber);
@@ -31,24 +33,48 @@ window.onload = function() {
 	var blue = Math.floor((Math.random() * 12) + 1);
 	var red = Math.floor((Math.random() * 12) + 1);
 
+		//setting value attribute of each crystal to the random number generated
+		$("#green").attr({
+	        "value" : green,
+	    });
 
-	$("#green").click();
-	$("#yellow").click();
-	$("#blue").click();
-	$("#red").click();
+	    $("#yellow").attr({
+	        "value" : yellow,
+	    });
 
-	console.log(randomNumber);
-	console.log(green);
-	console.log(yellow);
-	console.log(blue);
-	console.log(red);
+	    $("#blue").attr({
+	        "value" : blue,
+	    });
+
+	    $("#red").attr({
+	        "value" : red,
+	    });
+
+	//trying to assign a value of 1 to a random crystal if no crystal equals 1
+	/*var crystals = [green, yellow, blue, red];
+		if (green !== 1 && yellow !== 1 && blue !== 1 && red !== 1) {
+			var one = crystals[Math.floor(Math.random() * crystals.length)];
+			console.log(one);
+		}*/
+
+	$(".crystal").on("click", function() {
+		console.log(this);
+		var value = $(this).attr('value');
+		console.log(value);
+		var total = points + value;
+		$("#displayPoints").html("Your Total: " + (total));
+	});
+
+	
+
+
+
+	console.log(points);
 
 
 
 
-//$("#crystals").on("click", function() {
 
-//}
 
 
 
